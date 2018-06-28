20dd410ce74ba67c98831fcdb93c3d1f74de30f3
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-28 17:30:59 -0600

    Posting array of words happy path

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,4 +1,15 @@
 class WordsController < ActionController::API
   def create
+    word_params.each do |word|
+      Word.create(text: word)
+    end
+    render status: 201
   end
+
+  private
+
+  def word_params
+    params.require(:words)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628231644) do
+ActiveRecord::Schema.define(version: 20180628232256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "words", force: :cascade do |t|
-    t.string "word"
+    t.string "text"
   end
 
 end
