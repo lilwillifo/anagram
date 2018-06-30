55b9bfe7c13a7ec163b22240ff32faf46dbaa859
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-30 12:20:29 -0600

    break out spec files

---
+++ spec/requests/words_request_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'Words API' do
+  it 'posts word(s) to database' do
+    params = { "words": ["read", "dear", "dare"] }
+    post '/words', params: params
+
+    expect(response).to be_success
+    expect(Word.count).to eq(params[:words].count)
+  end
+  it 'can delete a word and returns 204 status code' do
+    pizza = Word.create(text: 'pizza')
+    sorbet = Word.create(text: 'sorbet')
+
+    expect(Word.count).to eq(2)
+
+    delete "/words/#{pizza.text}"
+
+    expect(response.status).to eq(204)
+    expect(Word.count).to eq(1)
+  end
+  it 'can delete all words and returns 204' do
+    pizza = Word.create(text: 'pizza')
+    sorbet = Word.create(text: 'sorbet')
+
+    expect(Word.count).to eq(2)
+
+    delete "/words"
+
+    expect(response.status).to eq(204)
+    expect(Word.count).to eq(0)
+  end
+end
