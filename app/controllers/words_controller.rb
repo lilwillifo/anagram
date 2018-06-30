c4ffca8a9aa763116e72822175d4afac0b60e757
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-30 11:46:03 -0600

    updating readme

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,13 +3,19 @@
 # @author Margaret Williford
 
 class WordsController < ActionController::API
+  # adds a new word to the database and renders JSON of the added word
+  #
+  # @return [JSON]
   def create
     word_params.each do |word|
       Word.create(text: word)
     end
-    render json: Word.all, status: 201
+    render json: Word.last, status: 201
   end
 
+  # Removes a word(found by text params) from dictionary. It will no longer show up
+  # as an anagram for other words.
+  # @return [status] shows the user a 204 status to confirm word was deleted.
   def destroy
     if params[:text]
       word = Word.find_by(text: params[:text])
